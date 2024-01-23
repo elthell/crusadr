1c3143b84cd7ba4c2d9c3312718e4879f1b44433
elthell <eleanor.thell@gmail.com>
2024-01-23 07:46:14 -0600

    add virtuals to army + crusade model

--- server/models/Army.js
+++ server/models/Army.js
@@ -1,41 +1,58 @@
 const { Schema, model } = require("mongoose");
 const unitSchema = require("./subdocs/Unit");
 
-const armySchema = new Schema({
-  faction: {
-    type: String,
-    required: true,
-  },
-  factionRules: {
-    type: String,
-  },
-  detachment: {
-    type: String,
-    required: true,
-  },
-  detachmentRules: {
-    type: String,
-  },
-  strategems: [
-    {
-      stratName: {
-        type: String,
-        required: true,
-      },
-      stratCost: {
-        type: Number,
-        required: true,
-        default: 0,
-      },
-      stratText: {
-        type: String,
-        required: true,
-        minlength: 1,
-        maxlength: 250,
-      },
+const armySchema = new Schema(
+  {
+    faction: {
+      type: String,
+      required: true,
     },
-  ],
-  units: [unitSchema],
+    factionRules: {
+      type: String,
+      minlength: 1,
+      maxlength: 250,
+    },
+    detachment: {
+      type: String,
+      required: true,
+    },
+    detachmentRules: {
+      type: String,
+      minlength: 1,
+      maxlength: 250,
+    },
+    strategems: [
+      {
+        stratName: {
+          type: String,
+          required: true,
+        },
+        stratCost: {
+          type: Number,
+          required: true,
+          default: 0,
+        },
+        stratText: {
+          type: String,
+          required: true,
+          minlength: 1,
+          maxlength: 250,
+        },
+      },
+    ],
+    units: [unitSchema],
+  },
+  // set this to use virtual below
+  {
+    toJSON: {
+      virtuals: true,
+    },
+  }
+);
+
+// another field for unit count when querying an army
+armySchema.virtual("unitCount").get(function () {
+  return this.units.length;
 });
 
 const Army = model("Army", armySchema);
--- server/models/Crusade.js
+++ server/models/Crusade.js
@@ -1,34 +1,47 @@
 const { Schema, Types } = require("mongoose");
 const battleSchema = require("./subdocs/Battle");
 
-const crusadeSchema = new Schema({
-  setting: {
-    type: String,
-    required: true,
-    minlength: 1,
-    maxlength: 250,
-  },
-  startDate: {
-    type: Date,
-    default: Date.now(),
-  },
-  users: [
-    {
-      userName: {
-        type: Schema.Types.ObjectId,
-        ref: "User",
-      },
-      userArmy: {
-        type: Schema.Types.ObjectId,
-        ref: "Army",
-      },
-      userPoints: {
-        type: Number,
-        default: 0,
-      },
+const crusadeSchema = new Schema(
+  {
+    setting: {
+      type: String,
+      required: true,
+      minlength: 1,
+      maxlength: 250,
     },
-  ],
-  battles: [battleSchema],
+    startDate: {
+      type: Date,
+      default: Date.now(),
+    },
+    users: [
+      {
+        userName: {
+          type: Schema.Types.ObjectId,
+          ref: "User",
+        },
+        userArmy: {
+          type: Schema.Types.ObjectId,
+          ref: "Army",
+        },
+        userPoints: {
+          type: Number,
+          default: 0,
+        },
+      },
+    ],
+    battles: [battleSchema],
+  },
+  // set this to use virtual below
+  {
+    toJSON: {
+      virtuals: true,
+    },
+  }
+);
+
+// another field for battle count when querying a crusade
+crusadeSchema.virtual("battleCount").get(function () {
+  return this.battles.length;
 });
 
 const Crusade = model("Crusade", crusadeSchema);
